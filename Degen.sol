10c2af26580a376595907506835149b72a010a0e
imadii024 <143532491+imadii024@users.noreply.github.com>
2024-03-23 01:56:03 +0530

    Update Degen.sol

--- Degen.sol
+++ Degen.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Points is ERC20, Ownable {
+contract Degen is ERC20, Ownable {
 
     enum Level { Level1, Level2, Level3 }
 
